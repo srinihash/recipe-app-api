ebbfb6b96b0e2b31ba54f7166496e40309f1293e
srinivas <srinivas.js.24@gmail.com>
2020-11-17 15:27:43 +0530

    Added users API (create user)

---
+++ app/user/tests/test_users_api.py
@@ -0,0 +1,43 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+from rest_framework.test import APIClient
+from rest_framework import status
+
+
+CREATE_USER_URL = reverse('user:create')
+
+
+def create_user(**params):
+    return get_user_model().objects.create_user(**params)
+
+
+class PublicUserApiTests(TestCase):
+    """Tests the users API (public)"""
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_create_valid_user_success(self):
+        """Test valid user creation with success"""
+        payload = {
+            "email": "test@email.com",
+            "password": "testpasswd",
+            "name": "testuser"
+        }
+        res = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        user = get_user_model().objects.get(**res.data)
+        self.assertTrue(user.check_password(payload["password"]))
+        self.assertNotIn("password", res.data)
+
+    def test_user_exists(self):
+        """Tests for behavior towards duplicate user creation"""
+        payload = {"email": "test@email.com", "password": "testpasswd"}
+        create_user(**payload)
+
+        res = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
